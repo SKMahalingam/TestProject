d8e726a861157cc4e74e24300fbaa8d69c1ce1bc
Mahalingam <Mahalingam@Mahalingam-PC>
2018-08-30 14:33:18 +0530

    Add Sysout function

--- Gitdemoo/src/com/git/demo/Springboot.java
+++ Gitdemoo/src/com/git/demo/Springboot.java
@@ -3,8 +3,7 @@ package com.git.demo;
 public class Springboot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to spring BOOT");
 	}
 
 }
